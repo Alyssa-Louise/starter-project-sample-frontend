8e20a347ff18226017d06d283a01e7a0e16f59fc
Alyssa Amadora <aamadora@vendasta.com>
2025-05-09 10:45:46 -0600

    changed app.routes.ts to accommodate a landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router'; // Import Routes
 import { FeedbackFormComponent } from './feedback-form/feedback-form.component';
 import { FeedbackListComponent } from './feedback-list/feedback-list.component';
+import { LandingComponent } from './landing/landing.component';
 
 export const routes: Routes = [ // Define and export the routes array
   { path: 'submit', component: FeedbackFormComponent },
   { path: 'feedback', component: FeedbackListComponent },
-  { path: '', redirectTo: '/submit', pathMatch: 'full' }, // Redirect to submit form by default
-  { path: '**', redirectTo: '/submit' } // Redirect unknown paths to submit form
+  { path: '', component: LandingComponent},
+  { path: '**', redirectTo: '' } // This redirects any unknown paths to the root ('/')
 ];
