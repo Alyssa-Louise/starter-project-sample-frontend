0f821c6a490b5e57125be5330611ef48f2cdebea
Alyssa Amadora <aamadora@vendasta.com>
2025-05-12 08:06:43 -0600

    debug: added a printing block to print if the button was clicked

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
+  standalone: true, // Make sure this is true
   imports: [],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
-
+  // Add a method to handle button clicks
+  onButtonClick(buttonType: string) {
+    console.log(`${buttonType} button clicked!`);
+  }
 }
